47c5d535defe5d2378308bf2fdf858ef43d7f7dd
SwadhiSankar <swadhis14@gmail.com>
2024-09-06 10:05:58 +0530

    using close func and get rid of for loop and array

--- main.go
+++ main.go
@@ -14,22 +14,27 @@ func slowGreet(phrase string,doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan) //which operation takes longest
 }
 
 func main() {
-	dones := make([]chan bool,4)
-    // done := make(chan bool  ) //communication device
-	dones[0] = make(chan bool)
-	go greet("Nice to meet you!",dones[0])
-	dones[1] = make(chan bool)
-	go greet("How are you?",dones[1])
-	dones[2] = make(chan bool)
-	go slowGreet("How ... are ... you ...?",dones[2])
-	dones[3] = make(chan bool)
-	go greet("I hope you're liking the course!",dones[3])
+	// dones := make([]chan bool,4)
+    done := make(chan bool  ) //communication device
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you!",done)
+	// dones[1] = make(chan bool)
+	go greet("How are you?",done)
+	// dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?",done)
+	// dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!",done)
 
-	 for _, done := range dones{
-		<-done
-	 }
+	//  for _, done := range dones{
+	// 	<-done
+	//  }
+
+	for range done{
+		
+	}
 
 }
